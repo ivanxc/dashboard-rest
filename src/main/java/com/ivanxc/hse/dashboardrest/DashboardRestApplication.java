a70b066410c19494b871f9b55753ef522635cc19
Ivan <djsupersky@gmail.com>
2022-01-27 21:52:49 +0300

    init

---
+++ src/main/java/com/ivanxc/hse/dashboardrest/DashboardRestApplication.java
@@ -0,0 +1,13 @@
+package com.ivanxc.hse.dashboardrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashboardRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DashboardRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ivanxc/hse/dashboardrest/DashboardRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivanxc.hse.dashboardrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashboardRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
